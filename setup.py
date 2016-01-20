ca5506a0678f693b6ac013c54b24ffef6f65f1c1
amix <amix@amix.dk>
2016-01-20 19:50:43 -0200

    .

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='timezones',
-      version = '1.9.5',
+      version = '1.9.6',
       author="amix",
       author_email="amix@amix.dk",
       url="http://www.amix.dk/",
