dc0b2f958503779e82a08de811c8552b7ba9b1bf
Roman Imankulov <roman.imankulov@gmail.com>
2019-02-22 16:31:20 +0000

    Drop guess_timezone_by_javascript()
    It never worked anyway, because the formatted string returned by
    JavaScript is not standardized and depends on browser and OS.
    
    Canonical name of the timezone is returned by this:
    
    Intl.DateTimeFormat().resolvedOptions().timeZone
    
    Apparently, it's widespread enough to rely on this method now:
    
    https://caniuse.com/#search=Intl

--- timezones/test_timezones.py
+++ timezones/test_timezones.py
@@ -48,20 +48,9 @@ def test_guess_timezone():
     assert tz_utils.guess_timezone_by_ip('201.246.115.62', only_name=True) == None
 
 
-def test_guess_timezone_by_javascript():
-    asia_yekaterinburg = tz_utils.get_timezone('Asia/Yekaterinburg')
-    for i in xrange(2):  # to ensure that cache breaks nothing
-        assert tz_utils.guess_timezone_by_javascript('Tue Feb 01 2005 00:00:00 GMT+0500 (YEKT)') == asia_yekaterinburg
-        assert tz_utils.guess_timezone_by_javascript('Mon Aug 01 2005 00:00:00 GMT+0600 (YEKST)') == asia_yekaterinburg
-        assert tz_utils.guess_timezone_by_javascript('Mon Aug 01 2005 00:00:00 GMT+0600 (OOOPS)') == None
-
-    assert tz_utils.guess_timezone_by_javascript('Tue Feb 01 2005 00:00:00 GMT+0500 (YEKT)', True) == 'Asia/Yekaterinburg'
-    assert tz_utils.guess_timezone_by_javascript('Mon Aug 01 2005 00:00:00 GMT+0600 (YEKST)', True) == 'Asia/Yekaterinburg'
-    assert tz_utils.guess_timezone_by_javascript('Mon Aug 01 2005 00:00:00 GMT+0600 (OOOPS)', True) == None
-
-
 def test_get_timezonez():
     assert len(list(zones.get_timezones(only_us=True))) == 8
+
 
 @pytest.mark.parametrize('offset_str,tzname,verbose_name', zones._ALL_TIMEZONES)
 def test_valid_offset(offset_str, tzname, verbose_name):
