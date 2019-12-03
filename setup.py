0010152f0de74d6575c036dd16ac77414bcc9f70
Thomas Jost <thomas@doist.com>
2019-12-03 01:04:02 +0100

    Bump to 2.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='timezones',
-    version='2.0.4',
+    version='2.0.5',
     author="Doist Developers",
     author_email="dev@doist.com",
     url="https://doist.com",
