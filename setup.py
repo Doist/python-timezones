6d98ebdd88a86e640b2f0937fbbe6033d7e87c67
Felipe Bidu Rodrigues <felipe@felipevr.com>
2021-07-20 15:57:42 -0300

    Add setuptools_scm before anything else

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     ],
     packages=["timezones"],
     use_scm_version=True,
-    setup_requires=["setuptools_scm==5.0.2"],
+    setup_requires=["setuptools_scm"],
     install_requires=["geoip2", "pytz", "future"],
     platforms=["Any"],
     license="MIT",
